ad5bfb2d48edebaad5cd52892ff0dd66ae49d5f7
Anthony Lewis <me@anthonylewis.com>
2011-06-23 20:55:26 -0500

    More tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  validates :author, :presence => true
+  validates :body, :presence => true
 end
--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -5,4 +5,15 @@ class CommentTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
+  test "author is required" do
+    comment = Comment.new :body => "Hello, World!"
+    assert !comment.valid?
+  end
+
+  test "body is required" do
+    comment = Comment.new :author => "Tony"
+    assert !comment.valid?
+  end
 end
+
